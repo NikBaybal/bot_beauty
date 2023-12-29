87501689273a5a043c9cc84e9e36bed13e48c029
Pavel <nikipparap@mail.ru>
2023-12-29 22:46:15 +0900

    first add

---
+++ functions.py
@@ -0,0 +1,10 @@
+from aiogram.types import InputMediaPhoto
+from aiogram.types import FSInputFile
+
+def img_answer(path:str):
+    file_in=FSInputFile(path, "rb")
+    return file_in
+
+def img(path:str,text:str)->InputMediaPhoto:
+    file_in=FSInputFile(path, "rb")
+    return InputMediaPhoto(media=file_in, caption=text, parse_mode = 'HTML')
